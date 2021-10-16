8d37f8521a4846ced9413acd4a10145e5ec4ee6f
dimethyehara <82025722+dimethyehara@users.noreply.github.com>
2021-10-16 20:23:44 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,8 @@
+print("THIS TOOL MADE BY USING t.me/SingleDevelopers SSH CREATOR API
+      
+        FULL CREDIT GOES TO MALITH RUKSHAN
 
+      API Documentation (https://telegra.ph/SSH-Creator-API-10-16) 
+      API tutorial (https://t.me/SingleDevelopers/613)
+      TERMUX TOOL BY - @Uma_weerasinghe5
+      ")
